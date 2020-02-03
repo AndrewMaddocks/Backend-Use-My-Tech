09129281ca27acde88858812f7cbd2c2a1289737
Andrew Maddocks <andrewmadd@icloud.com>
2020-02-03 13:56:26 -0700

    added image string

--- data/migrations/20200201224332_tech.js
+++ data/migrations/20200201224332_tech.js
@@ -2,10 +2,16 @@ exports.up = function(knex) {
   return knex.schema
     .createTable("tech", tbl => {
       tbl.increments();
+
       tbl.string("name").notNullable();
       tbl.string("description");
       tbl.string("price");
       tbl.string("rented").defaultTo(false);
+      tbl
+        .string("image")
+        .defaultTo(
+          "https://static9.depositphotos.com/1575949/1203/v/950/depositphotos_12034707-stock-illustration-smiley-icon.jpg"
+        );
       tbl
         .string("owner_id")
         .unsigned()
